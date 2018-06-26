a027030ca975cc7fe99578d91f8ac07cc0b7f4b5
Alex Kelley <adkelley@mac.com>
2018-06-25 18:16:31 -0700

    03 refactor

--- 03_CSS_Variables/src/Main.purs
+++ 03_CSS_Variables/src/Main.purs
@@ -7,13 +7,12 @@ import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
 import Effect (Effect)
 import Effect.Console (error)
-import Web.DOM.Internal.Types (NodeList)
 import Web.DOM.Node (toEventTarget)
 import Web.DOM.NodeList (toArray)
 import Web.DOM.ParentNode (querySelectorAll)
 import Web.Event.Event (currentTarget)
 import Web.Event.EventTarget (addEventListener, eventListener)
-import Web.Event.Internal.Types (Event, EventTarget)
+import Web.Event.Internal.Types (Event)
 import Web.HTML (HTMLInputElement, window)
 import Web.HTML.HTMLDocument (toParentNode)
 import Web.HTML.HTMLInputElement (fromEventTarget, name, value)
@@ -21,6 +20,7 @@ import Web.HTML.Window (document)
 
 foreign import datasetSizing :: HTMLInputElement → Effect String
 foreign import setStyleProperty :: String → String → String → Effect Unit
+
 
 handleUpdate :: Event → Effect Unit
 handleUpdate e = do
@@ -35,15 +35,11 @@ handleUpdate e = do
         Nothing → error "no HTMLInputelement"
     Nothing → error "this shouldn't happen"
 
-toEventTargets :: NodeList → Effect (Array EventTarget)
-toEventTargets nodeList = do
-  nodes ← toArray nodeList
-  pure $ map toEventTarget nodes
 
 main :: Effect Unit
 main = do
   doc ← map toParentNode (window >>= document)
-  inputs ← querySelectorAll (wrap ".controls input") doc >>= toEventTargets
+  inputs ← querySelectorAll (wrap ".controls input") doc >>= toArray
   el ← eventListener handleUpdate
-  for_ inputs $ addEventListener (wrap "change") el false
-  for_ inputs $ addEventListener (wrap "mousemove") el false
+  for_ inputs $ \x → addEventListener (wrap "change") el false (toEventTarget x)
+  for_ inputs $ \x → addEventListener (wrap "mousemove") el false (toEventTarget x)
